2a1c62f476d0f030efba6b7082a2eb834595c737
Zsombor Bérces <32767685+d7p4n4@users.noreply.github.com>
2019-12-14 19:38:43 +0100

    Add project files.

---
+++ CSFlowDocumentTry1/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CSFlowDocumentTry1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Person> personList = new List<Person>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonAction(object subject, RoutedEventArgs e)
+        {
+            Person person = new Person()
+            {
+                name = uiTextBoxNev.Text.ToString(),
+                age = uiTextBoxKor.Text.ToString()
+            };
+
+            personList.Add(person);
+            uiListView.Items.Add(person);
+
+            uiTextBoxKor.Text = "";
+            uiTextBoxNev.Text = "";
+
+        }
+
+        public class Person
+        {
+            public string name { get; set; }
+            public string age { get; set; }
+        }
+    }
+}
